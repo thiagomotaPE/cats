a187332938060a9841f239c4686fcd706c739533
mota <thiagomotape@hotmail.com.br>
2023-12-03 14:46:45 -0300

    insert_typeOr
    comecei a adicionar o typeORM ao projeto

--- server/cats/src/app.module.ts
+++ server/cats/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
 import { PrismaService } from './infra/prisma/prisma.service';
-import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cats } from './cats/entities/cats.entity';
 
 @Module({
-  imports: [CatsModule, ConfigModule.forRoot()],
+  imports: [
+    CatsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'cats',
+      entities: [Cats],
+      synchronize: true,
+    }),
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
--- server/cats/src/cats/cats.module.ts
+++ server/cats/src/cats/cats.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 import { PrismaService } from 'src/infra/prisma/prisma.service';
+import { Cats } from './entities/cats.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cats])],
   controllers: [CatsController],
   providers: [CatsService, PrismaService],
 })
